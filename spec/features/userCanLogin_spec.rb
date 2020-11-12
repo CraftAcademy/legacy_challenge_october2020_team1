0f093d03677dcf15c315a0267e1816aa3cd35779
KyunginNa <72070417+KyunginNa@users.noreply.github.com>
2020-11-12 12:52:49 +0100

    Update userCanLogin_spec.rb
    - Added a sad path about typing invalid password

--- spec/features/userCanLogin_spec.rb
+++ spec/features/userCanLogin_spec.rb
@@ -28,13 +28,25 @@ feature 'User can login' do
 
   context 'sadpath: user types a wrong email address' do
     before do
-      fill_in "Email", with: 'user123.email'
+      fill_in "Email", with: ' '
       fill_in "Password", with: user.password
       click_on "Log in"
     end
 
-    it 'User should see an error message' do
+    it 'User should see an error message for invalid email' do
       expect(page).to have_content 'Invalid Email or password.'
     end  
+  end
+  
+  context 'sadpath: user types a wrong passsword' do
+    before do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: ' '
+      click_on "Log in"
+    end
+  
+    it 'User should see an error message for invalid password' do
+      expect(page).to have_content 'Invalid Email or password.'
+    end 
   end
 end
