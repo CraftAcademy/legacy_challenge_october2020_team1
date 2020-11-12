520ada96db2de3596b51199e9d82669ecb1194cb
Sanne <sannelindholm@hotmail.com>
2020-11-12 17:38:57 +0100

    Refactoring code to fix test but test fails.

--- spec/features/userCanSendMessages_spec.rb
+++ spec/features/userCanSendMessages_spec.rb
@@ -1,42 +1,28 @@
-feature 'User can send message' do
-  let(:user) { FactoryBot.create(:user) }
+feature 'User can' do
+  let(:sender) { FactoryBot.create(:user, email: 'sender@mail.com') }
   before do
-  login_as(user, scope: :user)
+  login_as(sender, scope: :user)
   visit root_path
   click_on 'Inbox'
   click_on 'Compose'   
 end
 
-  context "Successfully send message [Happy Path]" do
+  context "Successfully send message [Happy Path]" do |name|
   let(:sender) { FactoryBot.create(:user, email: 'sender@mail.com') }
   let(:receiver) { FactoryBot.create(:user, email: 'receiver@mail.com') }
     before do
-      sender.send_message(sender, 'Lorem ipsum...', 'Subject')
-      # select('Lucia', :from => 'Receipie')
-      # fill_in "Subject", with: "Hej Hi Hello "
-      # fill_in "Type your message here", with: "Random stuff"
+      # visit new_conversation_path
+      # sender.send_message(receiver, 'Lorem ipsum...', 'Subject')
+      select_from_chosen(receiver.name, from: conversation_recipients)
+      # @receiver = User.find_by(name: name)
+      # @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
+      fill_in 'Subject', with: 'user712'
+      fill_in 'Message', with: 'user712@test.com'
       click_on "Send Message" 
     end
   end
 
     it 'User should see error message' do
-      expect(current_path).to have_content "Your message was successfully sent!"
+      expect(page).to have_content "Your message was successfully sent!"
     end
 end
-
-
-  #   it 'User should see success message' do
-  #     fill_in 'Name', with: 'user712'
-  #     fill_in 'Email', with: 'user712@test.com'
-  #     fill_in 'Password', with: '12345678'
-  #     fill_in 'Password confirmation', with: '12345678'
-  #     click_on 'Create'
-  #     expect(page).to have_content "Welcome! You have signed up successfully."
-  #   end
-  # end
-
-
-  #   let(:user) { FactoryBot.create(:user) }
-# before do
-#   login_as(user, scope: :user)
-#   visit root_path
