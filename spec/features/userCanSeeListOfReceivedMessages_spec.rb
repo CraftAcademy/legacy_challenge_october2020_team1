1b7496afc852cccf7574ef050d8482839b3418bb
swardbladpaulo <paulo@swardblads.se>
2020-11-12 19:38:02 +0100

    added test for user can see a recived message, test passed

--- spec/features/userCanSeeListOfReceivedMessages_spec.rb
+++ spec/features/userCanSeeListOfReceivedMessages_spec.rb
@@ -1,15 +1,22 @@
+require 'rails_helper'
+
 feature 'User can see list of recived messages' do
-  let(:user) {FactoryBot.create(:user)}
+  let(:sender) {FactoryBot.create(:user, name: 'sender', email: 'hellosender@abc.com')}
+  let(:receiver) {FactoryBot.create(:user)}
   
   before do
-    login_as(user, scope: :user)
+    login_as(receiver, scope: :user)
     visit root_path
-    binding.pry
+    sender.send_message(receiver, 'some example email from the sender', 'Subjec')
     click_on 'Inbox'
   end
 
   it 'Happypath user are redirected to inbox page' do
     expect(current_path).to eq mailbox_inbox_path
+  end
+
+  it 'Happypath user can see a recived message' do
+    expect(page).to have_content 'some example'
   end
 
 end
