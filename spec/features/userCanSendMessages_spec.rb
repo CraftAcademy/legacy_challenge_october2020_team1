8ba417a618b66ef67982f0fa41a5e78c5a111733
Sanne <sannelindholm@hotmail.com>
2020-11-13 15:49:44 +0100

    Adding an error message if message lacks recipient, subject or message in Create method in conversation_controller. Adding sad path to test with passes.

--- spec/features/userCanSendMessages_spec.rb
+++ spec/features/userCanSendMessages_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 feature 'User can' do
   let(:sender) { FactoryBot.create(:user) }
   let(:receiver) { FactoryBot.create(:user, name: 'Mr.sender', email: 'helloworld@mail.com') }
@@ -19,9 +17,24 @@ feature 'User can' do
       fill_in 'conversation_body', with: 'user712@test.com'
       click_on "Send Message" 
     end
-
+    
     it 'User should see successful message' do
       expect(page).to have_content "Your message was successfully sent!"
+    end
+  end
+
+  context "Unsuccessfully send message [Sad Path]" do
+    before do
+      visit new_conversation_path
+      select 'MyString', from: 'conversation_recipients'
+      fill_in 'conversation_subject', with: ' '
+      fill_in 'conversation_body', with: ' '
+      click_on "Send Message" 
+    end
+
+
+    it 'User should see error message' do
+      expect(page).to have_content "There was an error, your message was not sent!"
     end
   end
 end
